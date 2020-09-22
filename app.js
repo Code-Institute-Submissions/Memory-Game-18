9a9d1a3d1ae0745b492d793a74673490a5e85a2f
Tanabodee Iam Muang <tanabodee.iammuang@outlook.com>
2020-09-22 18:35:10 +0000

    added css for animation.Added matching, reset and  shuffle function in app.js

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle('flip');
+if (lockBoard) return;
+if (this === firstCard) return;
+
+this.classList.add('flip');
+
+if (!hasFlippedCard) {
+// click 1
+hasFlippedCard = true;
+firstCard = this;
+
+return;
 }
+
+ // click 2
+secondCard = this;
+
+checkForMatch();
+}
+
+function checkForMatch() {
+let isMatch = firstCard.dataset.dog === secondCard.dataset.dog;
+isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+firstCard.removeEventListener('click', flipCard);
+ secondCard.removeEventListener('click', flipCard);
+
+resetBoard();
+ }
+
+function unflipCards() {
+lockBoard = true;
+setTimeout(() => {
+firstCard.classList.remove('flip');
+secondCard.classList.remove('flip');
+
+ resetBoard();
+}, 1500);
+ }
+function resetBoard() {
+[hasFlippedCard, lockBoard] = [false, false];
+ [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+cards.forEach(card => {
+let randomPos = Math.floor(Math.random() * 20);
+card.style.order = randomPos;
+});
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
