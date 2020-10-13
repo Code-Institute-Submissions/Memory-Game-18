c27bc03d6bf1de57cb601c2f52776a6aee54c098
Tanabodee Iam Muang <tanabodee.iammuang@outlook.com>
2020-10-13 21:18:07 +0000

    renamed soumd files. Adjusted bg image and styling on html

--- assets/app.js
+++ assets/app.js
@@ -78,41 +78,22 @@ myAudio.onpause = function() {
     isPlaying = false;
 };
 
+//Loop function
+
+var x = document.getElementById("myAudio");
+
+function enableLoop() { 
+  x.loop = true;
+  x.load();
+} 
+
+function disableLoop() { 
+  x.loop = false;
+  x.load();
+} 
+
+function checkLoop() { 
+  alert(x.loop);
+} 
 /*Modal function*/
 
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-
-overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-        closeModal(modal)
-    })
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-}
